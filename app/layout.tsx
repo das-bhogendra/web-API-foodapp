9d80db1c6ea92ceb377586c4dd7c385e00ffcf70
das-bhogendra <bhogendrakumardas123@gmail.com>
2026-01-19 18:02:32 +0545

    insert some data in layout.ts

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import Header from "@/app/components/Header"; // import Header
 import "./globals.css";
+import { AuthProvider } from "@/context/AuthContext";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -26,10 +27,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
-        {/* Header is global */}
+        <AuthProvider>
+          {children}
+        </AuthProvider>
         <Header />
-        {/* Main content */}
-        {children}
       </body>
     </html>
   );
